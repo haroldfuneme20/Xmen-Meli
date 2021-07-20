5ead827a64041643676acca99a0fa75afe195717
Harold Funeme <h.funeme@uniandes.edu.co>
2021-07-20 17:27:40 -0500

    :white_check_mark: Unitest 61%

--- src/test/java/com/xmen/meli/hhfm/model/MutantRequestTest.java
+++ src/test/java/com/xmen/meli/hhfm/model/MutantRequestTest.java
@@ -1,12 +1,13 @@
 package com.xmen.meli.hhfm.model;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MutantRequestTest {
-    @Autowired
+    @Mock
     MutantRequest mutantRequest;
 
     @Test
